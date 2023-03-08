94e580ff49a7c73393f9a1bbf5cdf6625514b71c
kenjiuno <ku@digitaldolphins.jp>
2023-03-08 22:27:40 +0900

    - テスト追加

--- yPDFEditor.Tests/Tests/PdfEditTest.cs
+++ yPDFEditor.Tests/Tests/PdfEditTest.cs
@@ -94,6 +94,7 @@ namespace yPDFEditor.Tests.Tests
         [Test]
         [TestCase("title.pdf", "ZuABC.pdf")]
         [TestCase("ZuABC.pdf", "title.pdf")]
+        [TestCase("NoInfo.pdf", "title.pdf")]
         public void AppendPage(string pdf1, string pdf2)
         {
             var newPdf = TempUtil.Get(pdf1 + $".AppendPage.pdf");
